914e7a90cfa3e2233486a8c43532ae0d1954248d
Vansh Aggarwal <vanshaggarwal881@gmail.com>
2025-03-07 10:23:50 +0530

    made changes in statsection

--- src/components/assessment/StatSection.jsx
+++ src/components/assessment/StatSection.jsx
@@ -1,69 +1,29 @@
 import React from "react";
 import StatCard from "./StatCard";
+import {
+  HiOutlineStar,
+  HiOutlineChartBar,
+  HiOutlineCheck,
+} from "react-icons/hi";
 
 function StatSection({ rank, percentile, score, totalQuestions }) {
   const statsData = [
     {
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"
-          />
-        </svg>
-      ),
+      icon: <HiOutlineStar className="h-6 w-6" />,
       value: rank,
       label: "YOUR RANK",
       bgColor: "bg-yellow-100",
       textColor: "text-yellow-600",
     },
     {
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
-          />
-        </svg>
-      ),
+      icon: <HiOutlineChartBar className="h-6 w-6" />,
       value: `${percentile}%`,
       label: "PERCENTILE",
       bgColor: "bg-gray-100",
       textColor: "text-gray-600",
     },
     {
-      icon: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M5 13l4 4L19 7"
-          />
-        </svg>
-      ),
+      icon: <HiOutlineCheck className="h-6 w-6" />,
       value: `${score} / ${totalQuestions}`,
       label: "CORRECT ANSWERS",
       bgColor: "bg-green-100",
